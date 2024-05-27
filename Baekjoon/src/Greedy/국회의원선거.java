a0308dc50b0e61e8cf2ed0ed6f69b5215ac1083f
yumzen <yeom.ji.eun35@gmail.com>
2024-05-27 18:29:34 +0900

    Greedy 알고리즘

---
+++ Baekjoon/src/Greedy/국회의원선거.java
@@ -0,0 +1,36 @@
+package Greedy;
+//1417 국회의원 선거
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 국회의원선거 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int me = Integer.parseInt(br.readLine());
+        Integer arr[] = new Integer[n-1];
+        int count = 0;
+
+        if(n==1) System.out.println(0);
+
+        for(int i =0; i<arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        while (true){
+            Arrays.sort(arr, Collections.reverseOrder()); //내림차순 정렬
+            if(arr[0]< me) break;
+            else{
+                me++;
+                arr[0]-=1;
+                count++;
+            }
+        }
+
+        System.out.print(count);
+    }
+}
