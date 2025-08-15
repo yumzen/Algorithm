1bf2bb7168eeef2ea95e374c16e5421bcf7861d0
yumzen <yeom.ji.eun35@gmail.com>
2025-08-15 15:01:45 +0900

    프로그래머스 의상 문제 풀이

--- Solved/src/Hash/의상.java
+++ Solved/src/Hash/의상.java
@@ -3,20 +3,17 @@ import java.util.*;
 
 public class 의상 {
     public int solution(String[][] clothes) {
-        Map<String, Integer> map = new HashMap<>();
-
-        // 1. 해시맵을 사용하여 각 의상 종류별 개수 저장
-        for (String[] cloth : clothes) {
-            String category = cloth[1];
-            map.put(category, map.getOrDefault(category, 0) + 1);
-        }
-
-        // 2. 조합의 개수 계산
         int answer = 1;
-        for (int count : map.values()) {
-            answer *= (count + 1); // (해당 종류의 의상을 선택하는 경우 + 선택하지 않는 경우)
+        HashMap<String, Integer> map = new HashMap<>();
+        // 종류 별로 해시 맵에 저장
+        for(String[] c: clothes){
+            map.put(c[1], map.getOrDefault(c[1], 0)+1);
         }
 
-        return answer - 1;
+        // 경우의 수 구하기
+        for(Integer v : map.values()){
+            answer*=(v + 1);
+        }
+        return answer-1;
     }
 }
