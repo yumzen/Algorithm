2351cdd68300c13cc5b6aee2f39e420bef40ffd1
yumzen <yeom.ji.eun35@gmail.com>
2025-08-15 15:42:02 +0900

    프로그래머스 베스트앨범 문제 풀이

--- Solved/src/Hash/베스트앨범.java
+++ Solved/src/Hash/베스트앨범.java
@@ -4,39 +4,34 @@ import java.util.*;
 
 public class 베스트앨범 {
     public int[] solution(String[] genres, int[] plays) {
-        // 1. 장르를 담은 해시 맵 생성
-        Map<String, Integer> genresMap = new HashMap<>();
+        List<Integer> answer = new ArrayList<>();
+        // 1. 장르 플레이 수 합산 & 장르 별 고유번호 및 플레이수 정리
+        HashMap<String, Integer> genreRank = new HashMap<>();
+        HashMap<String, List<int[]>> genreSong = new HashMap<>();
         for(int i = 0; i < genres.length; i++){
-            // genresMap에 장르의 플레이횟수를 담기
-            genresMap.put(genres[i], genresMap.getOrDefault(genres[i], 0)+plays[i]);
+            genreRank.put(genres[i], genreRank.getOrDefault(genres[i], 0)+plays[i]);
+            if(!genreSong.containsKey(genres[i])){
+                genreSong.put(genres[i], new ArrayList<>());
+            }
+            List<int[]> list = genreSong.get(genres[i]);
+            list.add(new int[]{i, plays[i]});
         }
 
-        // 2. 장르별 총 재생 횟수를 기준으로 정렬
-        List<String> sortedGenres = new ArrayList<>(genresMap.keySet());
-        sortedGenres.sort((a, b) -> genresMap.get(b) - genresMap.get(a));
+        // 2. 장르 순위 구하기
+        List<String> sortedGenres = new ArrayList<>(genreRank.keySet());
+        sortedGenres.sort((a, b) -> genreRank.get(b) - genreRank.get(a));
 
-        //장르 안에서, 가장 많이 재생된 노래의 고유 번호부터 sort해서 넣기 이떄, 재생수가 같으면 index 오름차순으로
-        List<Integer> answerList = new ArrayList<>();
-        int answerIndex = 0;
-        for(String genre: sortedGenres){
-            // 장르의 play 인덱스를 저장하는 과정
-            List<Integer> genrePlaysIndex = new ArrayList<>();
-            for(int i = 0; i < plays.length; i++){
-                //해당 장르라면,
-                if(genre.equals(genres[i])){
-                    genrePlaysIndex.add(i);
-                }
-            }
 
-            // 이제, genrePlaysIndex를 플레이횟수의 내림차순으로 정렬한다.
-            genrePlaysIndex.sort((a,b) -> plays[b] - plays[a]);
-
-            // 2개만 사용
-            for(int i = 0 ; i < genrePlaysIndex.size() && i < 2; i ++){
-                answerList.add(genrePlaysIndex.get(i));
+        // 3. 장르에서 곡 인기순 정렬
+        for(String g: sortedGenres){
+            List<int[]> list = genreSong.get(g);
+            list.sort((a, b) -> {
+                if(b[1] != a[1]) return b[1] - a[1];
+                return a[0] - b[0];});
+            for(int i = 0; i <list.size() && i<2; i++){
+                answer.add(list.get(i)[0]);
             }
         }
-        // List -> int[] 변환
-        return answerList.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
